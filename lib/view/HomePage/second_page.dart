c33ae8ef7fa27114411c66a16f44399808fd7053
rimuakter057 <rimu74688@gmail.com>
2024-09-10 12:58:44 +0600

    first commit

---
+++ lib/view/HomePage/second_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
