5ea571ce4af7d0b5b02b1baf136bdbbcd9545a23
rimuakter057 <rimu.appdeveloper@gmail.com>
2025-03-26 12:34:59 +0600

    .....

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 
 import 'package:api_practice/view/HomePage/login_api_page.dart';
+import 'package:api_practice/view/practice_page/practice_page.dart';
 import 'package:flutter/material.dart';
-
-
 
 void main() {
 
@@ -16,7 +15,23 @@ class ApiPractice extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginApiPage(),
+      home: PracticePage(),
+    );
+  }
+}
+
+class HomePageN extends StatefulWidget {
+  const HomePageN({super.key});
+
+  @override
+  State<HomePageN> createState() => _HomePageNState();
+}
+
+class _HomePageNState extends State<HomePageN> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Rimur"),),
     );
   }
 }
---
+++ lib/view/practice_page/practice_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PracticePage extends StatelessWidget {
+  const PracticePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("data"),),
+    );
+  }
+}
